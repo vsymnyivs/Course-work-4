27e3fe60ae76f5506e0bc2950ec21ecc2a7e3f94
vsymnyivs <vsymnyivs@gmail.com>
2024-03-06 22:08:33 +0300

    add vacancy.py

---
+++ src/vacancy.py
@@ -0,0 +1,36 @@
+class Vacancy:
+    """
+    Форма для класса GetHeadHunter
+    """
+    def __init__(self, name, page):
+        """
+        Создание экземпляра класса Vacancy
+        """
+        self.__name = name
+        self.__page = page
+
+    @property
+    def name(self):
+        """
+        getter для приватного атрибута name
+        """
+        return self.__name
+
+    @property
+    def page(self):
+        """
+        getter для приватного атрибута page
+        """
+        return self.__page
+
+    def __str__(self):
+        """
+        Магический метод str
+        """
+        return f"{self.__name}"
+
+    def __repr__(self):
+        """
+        Магический метод repr
+        """
+        return f"{self.__class__.__name__}({self.__name, self.__page})"
